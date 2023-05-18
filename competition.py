444cd52c2702e14b2948456236c210c1e90d57ed
sraatgn <sarategoni18@gmail.com>
2023-05-18 12:14:21 +0200

    prepping for competition

---
+++ competition.py
@@ -0,0 +1,12 @@
+from dataset import *
+from utils import *
+from network import *
+from model_selection import list_model_type
+
+
+""" query and gallery retrieval """
+data_path = config['competition_code']['data_root']
+...
+
+""" """
+print(list_model_type)
