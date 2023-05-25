248c14f96d1a3908066e3853d78c92bcf277f81e
Marta Faggian <marta.faggian99@gmail.com>
2023-05-25 15:25:39 +0200

    updated competition.py
    Da sistemare (Marta)

--- competition.py
+++ competition.py
@@ -1,11 +1,12 @@
-from dataset import *
-from utils import *
-from network import *
-from model_selection import *
-from query import *
+# from dataset import *
+# from utils import *
+# from network import *
+# from model_selection import *
+# from query import *
 
+import json
 """ query and gallery retrieval """
-data_path = config['competition_code']['data_root']
+#data_path = config['competition_code']['data_root']
 
 """ here we want to select the model to submit """
 
@@ -13,17 +14,20 @@ data_path = config['competition_code']['data_root']
 ...
 
 ###to do
-with open("runs_recap_try.json", "r") as read_file:
+with open("runs_recap.json", "r") as read_file:
     data = json.load(read_file)
 
 minimum_loss = float('inf')
-best_models = {}
+best_model = {}
 
 for model in data:
     error = min(data[model]['test_loss'])
     if error < minimum_loss:
         minimum_loss = error
-        best_models = {model: data[model]}
+        best_model = {model: data[model]}
 
 print("Minimum loss:", minimum_loss)
-print("Best model:", best_models)
+print("Best model:", best_model)
+
+##MARTA##
+#sistemare per far sì che best model sia un dizionario di dizionari con i 4 modelli con i test error migliori (quindi modificare il min)
